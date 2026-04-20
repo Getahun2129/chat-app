66d7479df1616539804f7f321e6d53455c6c544b
Getahun2129 <getahun.endalamaw@southeasttech.edu>
2026-04-20 18:20:30 -0500

    change port number for the app

--- server.js
+++ server.js
@@ -20,6 +20,6 @@ io.on("connection", (socket) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+server.listen(8000, () => {
+    console.log("Server running on http://localhost:8000");
 });
